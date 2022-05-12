a726a633f931e41d3f0d49460d8c9612e351b1ec
diana-atas <dianadavidatas@gmail.com>
2022-05-12 21:19:39 +1200

    declare 2D array

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -5,6 +5,13 @@
 console.log('Whack-a-Mole!')
 
 // declare 2D array consisting 25 cells, 5 rows 5 columns
+
+// 0: (5) [0, 1, 2, 3, 4]
+// 1: (5) [5, 6, 7, 8, 9]
+// 2: (5) [10, 11, 12, 13, 14]
+// 3: (5) [15, 16, 17, 18, 19]
+// 4: (5) [20, 21, 22, 23, 24]
+
 // create function to get a random number
 // create function to use random number to pick random cell in 2D array
 // put the mole in the random cell
@@ -14,3 +21,19 @@ console.log('Whack-a-Mole!')
 // add mole to a new random cell
 
 // add sound effect when mole is clicked
+
+let grid = new Array(5)
+let h = 0
+
+// -- 2D ARRAY -- //
+for (let i = 0; i < grid.length; i++) {
+  grid[i] = []
+}
+
+for (let i = 0; i < grid.length; i++) {
+  for (let j = 0; j < grid.length; j++) {
+    grid[i][j] = h++
+  }
+}
+
+console.log(grid)
