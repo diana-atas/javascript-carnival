e709c9482504925eb6e0784d25207caabfb01f57
diana-atas <dianadavidatas@gmail.com>
2022-05-12 20:29:05 +1200

    add pseudocode

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,16 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole!')
+
+// declare 2D array consisting 25 cells, 5 rows 5 columns
+// create function to get a random number
+// create function to use random number to pick random cell in 2D array
+// put the mole in the random cell
+
+// create function called whackedMole
+// use onclick method to remove mole from current cell
+// add mole to a new random cell
+
+// add sound effect when mole is clicked
