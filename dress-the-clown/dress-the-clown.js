131b0dd5c8b8481d7a15b34bcfcce9cc30d1b665
diana-atas <dianadavidatas@gmail.com>
2022-05-13 23:12:46 +1200

    add pseudocode

--- dress-the-clown/dress-the-clown.js
+++ dress-the-clown/dress-the-clown.js
@@ -1,5 +1,26 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Dress The Clown!")
+console.log('Dress The Clown!')
+
+// declare variable to reference head - head
+// declare variable to reference body
+// declare variable to reference shoes
+
+// declare varialbe to store current number of the head image - headIndex
+// create function changeClownHead to change head image of the clown
+// use concat and headIndex to create new file path
+// assign new image to variable head
+
+// assign right arrow key to call function
+// increase headIndex by one so next image is used when function is called again
+// function should reset headIndex to zero once it reaches last image
+
+// assign left arrow key to call function
+// decrease headIndex by one so next image is used when function is called again
+// function should reset headIndex to zero once it reaches last image
+
+// declare variable to track which body part you are currently selecting - clothingIndex
+// assign up and down arrow keys to increase and decrease clothingIndex
+// refactor changeClownHead function to changeClothes function to change image for other body parts when respective clothingIndex is selected
