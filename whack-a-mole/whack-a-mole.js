7189f3ff92fd7c2bdd0e77e5ddf2cd20de5cef0e
diana-atas <dianadavidatas@gmail.com>
2022-05-13 16:16:46 +1200

    delete 2D array

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -4,16 +4,9 @@
 
 console.log('Whack-a-Mole!')
 
-// declare 2D array consisting 25 cells, 5 rows 5 columns
-
-// 0: (5) [0, 1, 2, 3, 4]
-// 1: (5) [5, 6, 7, 8, 9]
-// 2: (5) [10, 11, 12, 13, 14]
-// 3: (5) [15, 16, 17, 18, 19]
-// 4: (5) [20, 21, 22, 23, 24]
-
+// put TD in array
 // create function to get a random number
-// create function to use random number to pick random cell in 2D array
+// create function to use random number to pick random cell in array
 // put the mole in the random cell
 
 // create function called whackedMole
@@ -22,25 +15,14 @@ console.log('Whack-a-Mole!')
 
 // add sound effect when mole is clicked
 
-let grid = new Array(5)
-let h = 0
+// let grid = new Array(5)
+let cellCount = 25
 let cells = document.getElementsByTagName('TD')
-
-// -- 2D ARRAY -- //
-for (let i = 0; i < grid.length; i++) {
-  cells[i] = []
-}
-
-for (let i = 0; i < grid.length; i++) {
-  for (let j = 0; j < grid.length; j++) {
-    cells[i][j] = h++
-  }
-}
 
 console.log(cells)
 
 function showMole() {
-  let randomCell = getRandomInt(0, h - 1)
+  let randomCell = getRandomInt(0, cellCount - 1)
   let cell = document.getElementsByTagName('TD')[randomCell]
   let moleImage = document.getElementById('mole')
   moleImage.src = 'mole.PNG'
@@ -53,7 +35,6 @@ function showMole() {
 }
 
 showMole()
-// console.log('cell.moleImage: ' + cell.moleImage)
 
 function whackedMole(e) {
   let cell = e.target
@@ -61,9 +42,6 @@ function whackedMole(e) {
   showMole()
 }
 
-// activeMole.onclick = whackedMole
-
-// console.log('h: ' + h)
 // console.log(grid)
 // console.log(randomCell)
 
